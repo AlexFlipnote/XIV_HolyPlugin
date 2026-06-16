511275b405b3d4c2f0fa8cbed2aeba55351da132
AlexFlipnote <alexander.samuelsen.manso@gmail.com>
2026-06-16 21:04:40 +0200

    new things

---
+++ HoliestFluffiness/HousingBidRecord.cs
@@ -0,0 +1,37 @@
+using System;
+using SQLite;
+
+namespace HoliestFluffiness;
+
+public enum BidType { Private = 0, FC = 1 }
+
+[Table("housing_bids")]
+public class HousingBidRecord
+{
+    [PrimaryKey, AutoIncrement, Column("id")]
+    public int Id { get; set; }
+
+    [Column("character_key")]
+    public string CharacterKey { get; set; } = "";
+
+    [Column("district")]
+    public string District { get; set; } = "";
+
+    [Column("ward")]
+    public int Ward { get; set; }
+
+    [Column("plot")]
+    public int Plot { get; set; }
+
+    [Column("bid_number")]
+    public int BidNumber { get; set; }
+
+    [Column("bid_type")]
+    public BidType BidType { get; set; }
+
+    [Column("bid_cost")]
+    public long BidCost { get; set; }
+
+    [Column("bid_date")]
+    public DateTime BidDate { get; set; }
+}
